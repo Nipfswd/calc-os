118546b3dc79084c258de74ccabd49ae771b83b4
Vlad-ux-png <ggcc98765432110@gmail.com>
2026-05-22 10:11:32 +0300

    CalcOS

--- drivers/rtl8111/net.c
+++ drivers/rtl8111/net.c
@@ -1,18 +1,14 @@
-#include <cmos.h>
 #include <video.h>
-#include <mouse.h>
 #include <utils.h>
-#include <keyboard.h>
-#include <ata.h>
 #include <idt.h>
 #include <stdint.h>
-#include <fat.h>
 #include <pci.h>
 
 uint32_t mmio_base = 0;
+static uint32_t current_rx = 0;
 
-struct rtl8111_rx_desc rx_ring __attribute__((aligned(256)));
-uint8_t rx_buffer[1536] __attribute__((aligned(8)));
+struct rtl8111_rx_desc rx_ring[4] __attribute__((aligned(256)));
+uint8_t rx_buffer[4][1536] __attribute__((aligned(8)));
 
 struct rtl8111_tx_desc tx_ring __attribute__((aligned(256)));
 uint8_t tx_buffer[1536] __attribute__((aligned(8)));
@@ -31,19 +27,12 @@ void rtl8111_init() {
         return;
     }
 
-    uint32_t pci_command = pci_read_config_dword(dev->bus, dev->slot, dev->func, 1);
+    uint32_t pci_command = pci_read_config_dword(dev->bus, dev->slot, dev->func, 0x04);
     pci_command |= 0x0006; 
-    pci_write_config_dword(dev->bus, dev->slot, dev->func, 1, pci_command);
+    pci_write_config_dword(dev->bus, dev->slot, dev->func, 0x04, pci_command);
 
-    uint32_t bar2 = pci_read_config_dword(dev->bus, dev->slot, dev->func, 6);
-    
-    mmio_base = bar2 & 0xFFFFFFF0; 
-
-    print("MMIO Base: ", 1);
-    char buf[16];
-    htoa(mmio_base, buf);
-    print(buf, 1);
-    print("\n", 1);
+    uint32_t bar2 = pci_read_config_dword(dev->bus, dev->slot, dev->func, 0x18);
+    mmio_base = bar2 & 0xFFFFFFF0;
 
     mmio_write8(mmio_base + 0x37, 0x10); 
 
@@ -51,19 +40,25 @@ void rtl8111_init() {
         delay_ticks(25);
     }
 
-    rx_ring.status = 0xC0000600; 
-    rx_ring.vlan = 0;
-    rx_ring.buf_addr_low = (uint32_t)rx_buffer; 
-    rx_ring.buf_addr_high = 0;                 
-    *(volatile uint32_t*)(mmio_base + 0xE4) = (uint32_t)&rx_ring;
+    for (int i = 0; i < 4; i++) {
+        rx_ring[i].vlan = 0;
+        rx_ring[i].buf_addr_low = (uint32_t)rx_buffer[i]; 
+        rx_ring[i].buf_addr_high = 0;                 
+        rx_ring[i].status = 0x80000000 | 1536; 
+    }
+    rx_ring[3].status |= 0x40000000;
+
+    *(volatile uint32_t*)(mmio_base + 0xE4) = (uint32_t)&rx_ring[0];
     *(volatile uint32_t*)(mmio_base + 0xE8) = 0; 
 
-    *(volatile uint32_t*)(mmio_base + 0x44) = 0xF; 
+    *(volatile uint32_t*)(mmio_base + 0x44) = 0x0F | (1 << 7); 
 
     mmio_write8(mmio_base + 0x37, 0x08);
 
     tx_ring.buf_addr_low = (uint32_t)tx_buffer;
     tx_ring.buf_addr_high = 0;
+    tx_ring.vlan = 0;
+    tx_ring.status = 0x40000000;
 
     *(volatile uint32_t*)(mmio_base + 0x20) = (uint32_t)&tx_ring;
     *(volatile uint32_t*)(mmio_base + 0x24) = 0;
@@ -72,31 +67,51 @@ void rtl8111_init() {
 }
 
 void rtl8111_recv() {
-    while ((rx_ring.status & 0x80000000) != 0) {
-        delay_ticks(25);
+    if ((rx_ring[current_rx].status & 0x80000000) != 0) {
+        return;
     }
 
-    uint32_t packet_len = rx_ring.status & 0x3FFF;
-
-    print("First byte: ", 1);
+    print("Byte: ", 1);
     char buf[16];
-    htoa(rx_buffer[0], buf);
+    htoa(rx_buffer[current_rx][0], buf);
     print(buf, 1);
     print("\n", 1);
 
-    rx_ring.status |= 0x80000000;
+    rx_ring[current_rx].status = 0x80000000 | 1536;
+    if (current_rx == 3) {
+        rx_ring[current_rx].status |= 0x40000000;
+    }
+
+    current_rx = (current_rx + 1) % 4;
 }
 
-void rtl8111_send(uint8_t byte_to_send) {
-    tx_buffer[0] = byte_to_send;
-    
-    for(int i = 1; i < 60; i++) {
+void rtl8111_send(uint8_t byte_to_send, uint8_t* dest_mac) {
+    tx_buffer[0] = dest_mac[0]; 
+    tx_buffer[1] = dest_mac[1]; 
+    tx_buffer[2] = dest_mac[2]; 
+    tx_buffer[3] = dest_mac[3]; 
+    tx_buffer[4] = dest_mac[4]; 
+    tx_buffer[5] = dest_mac[5];
+
+    tx_buffer[6] = 0x00; 
+    tx_buffer[7] = 0xDE; 
+    tx_buffer[8] = 0xAD; 
+    tx_buffer[9] = 0xBE; 
+    tx_buffer[10] = 0xEF; 
+    tx_buffer[11] = 0x00;
+
+    tx_buffer[12] = 0x88;
+    tx_buffer[13] = 0xB5;
+
+    tx_buffer[14] = byte_to_send;
+
+    for(int i = 15; i < 60; i++) {
         tx_buffer[i] = 0;
     }
 
-    tx_ring.status = 0xF000003C;
+    tx_ring.status = 0x80000000 | 0x30000000 | 0x40000000 | 60;
 
-    *(volatile uint8_t*)(mmio_base + 0x38) = 0x01;
+    *(volatile uint8_t*)(mmio_base + 0x38) = 0x04;
 
     while ((tx_ring.status & 0x80000000) != 0) {
         delay_ticks(25);
